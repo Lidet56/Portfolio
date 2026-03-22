9b86d1792ea1f88f1978d43e43a3be5bbf85de80
Lidet56 <legesselidet@gmail.com>
2026-03-22 12:04:08 -0400

    finish portfolio fixes

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -24,9 +24,7 @@ const Skills = () => {
         <div className="sec-title text-center">
           <h2>My Skills</h2>
           <p>
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt <br />
-            mollit anim id est laborum Sed ut perspiciatis unde omnis.
+           As a college student, I have developed strong communication, teamwork, and problem solving skills through working with others in class, completing projects, and managing different responsibilities. I am also building technical skills through my studies, while continuing to improve my ability to adapt, stay organized, and learn quickly. In addition, my experience as a real estate agent helper helped me strengthen my customer service, professionalism, attention to detail, and ability to work with different people. Overall, my skills reflect both my academic growth and my real world experience.
           </p>
           <div className="line"></div>
         </div>
